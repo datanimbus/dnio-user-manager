fbc0d218f448883f9efad4aa503d23d72285ce69
kavi <kavi@appveen.com>
2022-03-29 14:26:26 +0530

    DEF1876

--- util/auth.js
+++ util/auth.js
@@ -225,7 +225,7 @@ function canAccessPath(req) {
 	if (compareURL('/rbac/{app}/user/utils/import/{id}', req.path) && _.intersection(req.user.appPermissions, ['PMUBC']).length > 0) {
 		return true;
 	}
-	if (compareURL('/rbac/app/{app}/removeUsers', req.path) && _.intersectionWith(req.user.appPermissions, ['PMU'], comparator).length > 0) {
+	if (compareURL('/rbac/{app}/user/utils/removeUsers', req.path) && _.intersectionWith(req.user.appPermissions, ['PMU'], comparator).length > 0) {
 		return true;
 	}
 	if (compareURL('/rbac/{app}/user/utils/closeAllSessions/{id}', req.path) && _.intersection(req.user.appPermissions, ['PMUA']).length > 0) {

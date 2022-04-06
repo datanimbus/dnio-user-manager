9b49a558ce35ca32feea2c97dc86b984f379d384
Jugnu Agrawal <jugnu@appveen.com>
2022-04-06 13:10:49 +0530

    version updated

--- app.js
+++ app.js
@@ -133,7 +133,7 @@ var options = {
 
 // The Swagger document (require it, build it programmatically, fetch it from a URL, ...)
 var spec = fs.readFileSync(path.join(__dirname, 'api/swagger/swagger.yaml'), 'utf8');
-var swaggerDoc = jsyaml.safeLoad(spec);
+var swaggerDoc = jsyaml.load(spec);
 
 swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
 

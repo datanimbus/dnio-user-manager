796388c506d564eeb7552c71e0729efdab73bd2e
Jugnu Agrawal <jugnu@appveen.com>
2022-05-16 17:28:40 +0530

    aws login fixed

--- scripts/push_ecr.sh
+++ scripts/push_ecr.sh
@@ -9,7 +9,7 @@ echo "****************************************************"
 echo "data.stack:user :: Pushing Image to ECR :: $ECR_URL/data.stack.user:$TAG"
 echo "****************************************************"
 
-aws ecr get-login --no-include-email
+$(aws ecr get-login --no-include-email)
 docker tag data.stack.user:$TAG $ECR_URL/data.stack.user:$TAG
 docker push $ECR_URL/data.stack.user:$TAG
 
